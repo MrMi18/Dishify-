4b56774cee9635581bb74926dd8a1f1a044ae3c7
Mr Mi <is8388508@gmail.com>
2025-01-12 17:50:35 +0530

    Offline error page update

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,111 +1,3 @@
-// import Cards from "./Cards"
-// import { API_URL} from "../utils/constant"
-// import { useEffect, useState } from "react"
-// import Shimmer from "./Shimmer"
-// import FoodItems from "./FoodItems"
-// import { Link, useParams } from "react-router-dom"
-// import Search from "./Search"
-// import useOnlineStatus from "../utils/useOnlineStatus"
-
-
-
-// const Main = () =>{
-   
-//     const [resData,setResData] = useState([]);
-//     const [allResData, setAllResData] = useState([]);
-//     const [moreResData, setMoreResData] = useState([]);
-//     const onlineStatus = useOnlineStatus();
-
-//     const availableRes  = () =>{
-//         const openRes =  resData && resData.filter((res) => res.info.availability.opened===true);
-//         setResData(openRes);
-//     };
-//     const reset = () =>{
-//         setResData(allResData);
-//     }
-
-//     useEffect(() =>{
-//         fetchData();
-//     },[]);
-
-//     const fetchData = async () =>{
-//         const data = await fetch(API_URL);
-//         const json = await data.json();
-
-       
-        
-//         const API_DATA = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-//         // console.log(API_DATA);
-//         setResData(API_DATA);
-//         setAllResData(API_DATA);
-
-//         const moreData =  json?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-//         moreData && setMoreResData(moreData);
-        
-//         if(!moreData){
-//           const altMoreData =  json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-//           setMoreResData(altMoreData);
-//         }
-
-//     };
-    
-//     if( resData && resData.length===0){
-//         return <Shimmer/>
-//     }
-//     if(moreResData && moreResData.length===0){
-//         return <Shimmer/>
-//     }
-    
-    
-    
-//    if(onlineStatus === false){
-//      return(  
-//      <div className="flex justify-center items-center h-72 w-screen">
-//         <h1 className="text-lg">Hey You are offline please connect with internet connection</h1>
-//      </div>)
-//    }
-
-        
-//      return (
-        
-//         <div style={{textAlign:"center"}}>
-//              <FoodItems />
-            
-//             <Search resData={allResData} setResData={setResData}/>
-//          <div style={{display:"flex", gap:"1.5rem", justifyContent:"center"}}>
-//             <button onClick={() =>{
-//               const topRes =   resData && resData.filter((res) => res.info.avgRating>4);
-//               setResData(topRes) 
-//              }} style={{padding:".5rem", border:"white", borderRadius:"2rem" }}>Top Restaurant</button>
-
-
-//             <button onClick={availableRes} style={{padding:".5rem", border:"white", borderRadius:"2rem"}}>Open Restaurant</button>
-//             <button onClick={reset} style={{padding:".5rem", border:"white", borderRadius:"2rem" }}>Reset Filter</button>
-//          </div>
-           
-             
-//             <div className="flex justify-evenly flex-wrap w-10/12 mx-auto p " >
-//               {
-                
-//                 resData && resData.map(data => <Link to={"/ResturantMenu/"+data.info.id} key={data.info.id}  style={{textDecoration:"none"}}>
-//                         <Cards apiData={data} /></Link>)
-//               }
-               
-//             </div>
-//             <div className="flex justify-evenly flex-wrap w-10/12 mx-auto " >
-//               {
-                
-//                 moreResData && moreResData.map(data => <Link to={"/ResturantMenu/"+data.info.id} key={data.info.id}  style={{textDecoration:"none"}}>
-//                         <Cards apiData={data} /></Link>)
-//               }
-               
-//             </div>
-
-//         </div>
-//     )
-// }
-// export default Main;
-
 
 
 import Cards from "./Cards";
@@ -116,6 +8,7 @@ import FoodItems from "./FoodItems";
 import { Link } from "react-router-dom";
 import Search from "./Search";
 import useOnlineStatus from "../utils/useOnlineStatus";
+import { toast } from "react-toastify";
 
 
 const Main = () => {
@@ -159,7 +52,7 @@ const Main = () => {
          
     }catch (error) {
         console.error("Error during fetch:", error);
-        alert("Failed to fetch data. Please try again later.");
+        // toast.warning("Failed to fetch data. Please try again later.");
     }
     };
     
@@ -169,10 +62,22 @@ const Main = () => {
     
     if (onlineStatus === false) {
         return (
-            <div className="flex justify-center items-center h-72 w-10/12
-            ">
-                <h1 className="text-lg">Hey You are offline please connect with internet connection</h1>
+            <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br bg-black text-white gap-2 mt-20">
+            <div className="max-w-lg text-center shadow-lg p-8 bg-white bg-opacity-10 rounded-lg backdrop-blur-md">
+                <h1 className="text-7xl font-extrabold mb-4 text-white drop-shadow-md">
+                    Oops!
+                </h1>
+                <p className="text-lg font-medium mb-6 text-white">
+                    Look like you are offline please check your internet connection.
+                </p>
+                <a
+                    href="/"
+                    className="mt-8 inline-block bg-white text-red-500 font-bold py-3 px-6 rounded-lg shadow-md hover:bg-gray-100 hover:shadow-lg transition duration-300"
+                >
+                    No internet
+                </a>
             </div>
+        </div>
         );
     }
 
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,37 +1,32 @@
-// import { useState } from "react";
 
-// const Search = ({resData,setResData}) =>{
-//     // console.log("props wala ",resData);
-//     const [searchText,setSearchText] = useState("");
-//     console.log(searchText);
-//     return(
-//         <div style={{padding:"1.5rem", width:"90%" }}>
-//             <input onChange={(e) =>{
-//                 setSearchText(e.target.value)
-//             }} value={searchText} style={{padding:"10px", width:"70%", marginLeft:"2rem" }} type="Text" placeholder="Search for resturant">
-//             </input>
-//             <button onClick={() =>{  
-//               const search =   resData.filter(
-//                 (res) => res?.info?.name.toLowerCase().includes(searchText.toLowerCase())
-                    
-//                 );
-//                 setResData(search)
-//                 // console.log("Search data",search);
-                                                
-//             }}
-//             style={{padding:"10px", width:"10%" , marginLeft:"2rem" }} >Search</button>
-//         </div>
-//     )
 
-// }
-
-// export default Search;
-
-import { useState } from "react";
+import { use, useState } from "react";
 import { toast } from "react-toastify";
 
 const Search = ({ resData, setResData,moreResData,setMoreResData ,allResData}) => {
     const [searchText, setSearchText] = useState("");
+    const [temp ,setTemp]= useState([]);
+
+
+   const handleSearch = () =>{
+    
+            const search = resData.filter(res => res?.info?.name.toLowerCase().includes(searchText.toLowerCase()) || 
+                 res?.info.cuisines.join(",").toLowerCase().includes(searchText.toLowerCase())|| []);
+                
+                const searchMore = moreResData.filter(res => res?.info?.name.toLowerCase().includes(searchText.toLowerCase())||[]);
+                
+                if(search.length===0 && searchMore.length===0){
+                    setMoreResData(allResData);
+                    setResData(allResData);
+                    toast.info("unable to find food and restaurant");
+                }else{
+                    setResData(search);
+                    setMoreResData(searchMore);
+                }
+   }
+
+
+
 
     return (
         <div className="p-4 w-90 ">
@@ -43,17 +38,8 @@ const Search = ({ resData, setResData,moreResData,setMoreResData ,allResData}) =
                 placeholder="Search for restaurant" 
                 
             />
-            <button onClick={() => {
-                const search = resData.filter(res => res?.info?.name.toLowerCase().includes(searchText.toLowerCase()));
-                setResData(search);
-                const searchMore = moreResData.filter(res => res?.info?.name.toLowerCase().includes(searchText.toLowerCase()));
-                setMoreResData(searchMore);
-                if(search.length===0 && searchMore.length===0){
-                    setMoreResData(allResData);
-                    setResData(allResData);
-                    toast.info("unable to find food and restaurant");
-                }
-            }} className="bg-orange-400 text-white px-4 py-2 rounded ml-2 hover:bg-orange-500">
+            <button onClick= {handleSearch}
+                 className="bg-orange-400 text-white px-4 py-2 rounded ml-2 hover:bg-orange-500">
                 Search
             </button>
         </div>
